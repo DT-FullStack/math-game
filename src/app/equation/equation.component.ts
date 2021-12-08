a0104d9dd40a8bfee0c38f0afc44af275ff6fd0d
bodywizard <david@bodywizardmedicine.com>
2021-12-07 19:33:46 -0600

    Added Reset button and Dynamic Operators

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,7 +1,7 @@
 // import { AbstractControlOptions, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from "@angular/forms";
-import { MathValidators } from '../math-validators';
+import { MathValidators, Operators, OperatorText } from '../math-validators';
 import { delay, filter, tap } from "rxjs/operators";
 
 @Component({
@@ -11,6 +11,14 @@ import { delay, filter, tap } from "rxjs/operators";
 })
 export class EquationComponent implements OnInit {
   secondsPerSolution: number = 0;
+  numberSolved: number = 0;
+  startTime: Date = new Date();
+  elapsedTime: number = 0;
+  timer = setInterval(() => {
+    this.secondsPerSolution = (new Date().getTime() - this.startTime.getTime()) / this.numberSolved / 1000;
+    this.elapsedTime++;
+  }, 1000)
+
   mathForm = this.formBuilder.group({
     a: [this.randomNumber()],
     b: [this.randomNumber()],
@@ -23,6 +31,16 @@ export class EquationComponent implements OnInit {
 
   get a() { return this.mathForm.value.a }
   get b() { return this.mathForm.value.b }
+  get operatorText(): string {
+    return OperatorText[this.operator];
+  }
+
+
+  operator: Operators = 0;
+
+  nextOperator(): Operators {
+    return Math.floor(Math.random() * 3);
+  }
 
   randomNumber() {
     return Math.floor(Math.random() * 10);
@@ -30,19 +48,33 @@ export class EquationComponent implements OnInit {
 
   refreshForm() {
     this.mathForm.setValue({ a: this.randomNumber(), b: this.randomNumber(), answer: '' })
+    this.operator = this.nextOperator();
+    const validator = MathValidators[Operators[this.operator]]('answer', 'a', 'b');
+    this.mathForm.setValidators(validator);
+  }
+
+  resetGame() {
+    clearInterval(this.timer);
+    this.numberSolved = 0;
+    this.startTime = new Date();
+    this.elapsedTime = 0;
+    this.refreshForm();
+    this.timer = setInterval(() => {
+      this.secondsPerSolution = (new Date().getTime() - this.startTime.getTime()) / this.numberSolved / 1000;
+      this.elapsedTime++;
+    }, 1000)
   }
 
   ngOnInit(): void {
-    const startTime = new Date();
-    let numberSolved = 0;
     this.mathForm.statusChanges.pipe(
       filter(value => value === 'VALID'),
       tap(() => {
-        numberSolved++;
-        this.secondsPerSolution = (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
+        this.numberSolved++;
       }),
       delay(300)
     ).subscribe(() => this.refreshForm())
+
+
   }
 
 }
--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -1,5 +1,12 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
+export enum Operators {
+  addition, subtraction, multiplication
+}
+export enum OperatorText {
+  '+', '-', 'x'
+}
+
 export class MathValidators {
   static addition(target: string, sourceOne: string, sourceTwo: string) {
     return (form: AbstractControl): ValidationErrors | null => {
@@ -8,6 +15,24 @@ export class MathValidators {
       const secondValue = form.value[sourceTwo];
       if (firstValue + secondValue === parseInt(targetValue)) return null;
       return { addition: true }
+    }
+  }
+  static subtraction(target: string, sourceOne: string, sourceTwo: string) {
+    return (form: AbstractControl): ValidationErrors | null => {
+      const targetValue = form.value[target];
+      const firstValue = form.value[sourceOne];
+      const secondValue = form.value[sourceTwo];
+      if (firstValue - secondValue === parseInt(targetValue)) return null;
+      return { subtraction: true }
+    }
+  }
+  static multiplication(target: string, sourceOne: string, sourceTwo: string) {
+    return (form: AbstractControl): ValidationErrors | null => {
+      const targetValue = form.value[target];
+      const firstValue = form.value[sourceOne];
+      const secondValue = form.value[sourceTwo];
+      if (firstValue * secondValue === parseInt(targetValue)) return null;
+      return { multiplication: true }
     }
   }
 }
