9aa7f2d745301d59ca142adb276bfa87826ea342
bodywizard <david@bodywizardmedicine.com>
2021-12-14 16:03:32 -0600

    Added more buttons

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from "@angular/forms";
 import { MathValidators, Operators, OperatorText } from '../math-validators';
 import { delay, filter, tap } from "rxjs/operators";
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-equation',
@@ -14,27 +15,27 @@ export class EquationComponent implements OnInit {
   numberSolved: number = 0;
   startTime: Date = new Date();
   elapsedTime: number = 0;
-  timer = setInterval(() => {
-    this.secondsPerSolution = (new Date().getTime() - this.startTime.getTime()) / this.numberSolved / 1000;
-    this.elapsedTime++;
-  }, 1000)
+  timer = null;
+
 
   mathForm = this.formBuilder.group({
-    a: [this.randomNumber()],
-    b: [this.randomNumber()],
+    a: 'a',
+    b: 'b',
     answer: [''],
   }, { validators: [MathValidators.addition('answer', 'a', 'b')] });
 
   constructor(
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.mathForm.disable();
+  }
 
   get a() { return this.mathForm.value.a }
   get b() { return this.mathForm.value.b }
   get operatorText(): string {
     return OperatorText[this.operator];
   }
-
+  get isRunning(): boolean { return this.mathForm.enabled };
 
   operator: Operators = 0;
 
@@ -54,15 +55,30 @@ export class EquationComponent implements OnInit {
   }
 
   resetGame() {
-    clearInterval(this.timer);
     this.numberSolved = 0;
     this.startTime = new Date();
     this.elapsedTime = 0;
+    this.secondsPerSolution = 0;
+    this.refreshForm();
+  }
+
+  startGame() {
+    clearInterval(this.timer);
+    this.mathForm.enable();
+    this.startTime = new Date();
     this.refreshForm();
     this.timer = setInterval(() => {
       this.secondsPerSolution = (new Date().getTime() - this.startTime.getTime()) / this.numberSolved / 1000;
       this.elapsedTime++;
     }, 1000)
+  }
+
+  stopGame() {
+    this.mathForm.disable();
+    this.mathForm.setValue({ a: 'a', b: 'b', answer: '' })
+    // this.mathForm.get('a').setValue('a');
+    // this.mathForm.get('b').setValue('b');
+    clearInterval(this.timer);
   }
 
   ngOnInit(): void {
